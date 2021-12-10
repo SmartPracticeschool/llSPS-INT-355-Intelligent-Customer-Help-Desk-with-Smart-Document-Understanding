ad4e5811c67caa553875b805f511f707d86630b3
Ansh Desai <anshdesai20@gmail.com>
2021-12-09 22:48:54 -0500

    Changes

---
+++ Desktop/DBMS/Project/demo.py
@@ -0,0 +1,68 @@
+import psycopg2
+from config import config
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+from urllib.error import URLError
+def connect(sql):
+    conn = None
+    try:
+        params = config()
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        print('Database connected')
+        cur.execute(sql)
+        df = cur.fetchall()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+    return df
+def page1():
+    try:
+        connect()
+        sql = ''
+        df = connect(sql)
+        countries = st.multiselect(
+            "Choose countries", list(df.index), ["China", "United States of America"]
+        )
+        if not countries:
+            st.error("Please select at least one country.")
+        else:
+            data = df.loc[countries]
+            data /= 1000000.0
+            st.write("### Gross Agricultural Production ($B)", data.sort_index())
+
+            data = data.T.reset_index()
+            data = pd.melt(data, id_vars=["index"]).rename(
+                columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
+            )
+            chart = (
+                alt.Chart(data)
+                .mark_area(opacity=0.3)
+                .encode(
+                    x="year:T",
+                    y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
+                    color="Region:N",
+                )
+            )
+            st.altair_chart(chart, use_container_width=True)
+    except URLError as e:
+        st.error(
+            """
+            **This demo requires internet access.**
+
+            Connection error: %s
+        """
+            % e.reason
+        )
+
+
+
+if __name__ == '__main__':
+    page1()
